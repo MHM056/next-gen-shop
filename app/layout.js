5df8b7b2dd6f183e0d7213cdb9eb84c0a96cc243
Mihail <mihailmarholev@gmail.com>
2025-04-21 22:12:51 +0300

    Updated layout

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import Header from "@/components/Header";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import CustomHead from "@/components/CustomHead";
 
 export const metadata = {
   title: "Create Next App",
@@ -19,9 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <CustomHead />
+      <body className="mini-h-screen">
+        <Header />
         {children}
       </body>
     </html>
