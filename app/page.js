8da91d2fd5d142a6a572090f7826dcc9800a8e0d
Mihail <mihailmarholev@gmail.com>
2025-05-11 21:13:48 +0300

    Updated home component
    - added newsletter component;
    - sanitized styles;

--- app/page.js
+++ app/page.js
@@ -1,32 +1,36 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./page.module.css";
+import Newsletter from "@/components/newsletter/Newsletter";
 
 export default function Home() {
     return (
-        <main className={styles.main}>
-            <img
-                className={styles["main-background"]}
-                src="https://public.readdy.ai/ai/img_res/1cd20f210a7471c9605fadedb9502475.jpg"
-                alt="Main Background"
-            />
-            <div className={styles.description}>
-                <h1>The Future of</h1>
-                <h1 className={styles["shopping-text"]}>Shopping</h1>
-                <h1>Is Here</h1>
-                <p>Experience next-generation e-commerce with our futuristic platform. Discover products that define
-                    tomorrow, today.
-                </p>
-                <a href="/shop" className={styles["cyan-button"]}>Shop Now</a>
-                <a href="/explore" className={styles["explore-button"]}>Explore AR</a>
-            </div>
-            <div className={styles["item-preview"]}>
+        <>
+            <main className={styles.main}>
                 <img
-                    className={styles["preview-image"]}
-                    src="https://public.readdy.ai/ai/img_res/a8dc01dfcfc98e07358a749458fa22b7.jpg"
-                    alt="Futuristic Headphones"
+                    className={styles["main-background"]}
+                    src="https://public.readdy.ai/ai/img_res/1cd20f210a7471c9605fadedb9502475.jpg"
+                    alt="Main Background"
                 />
-            </div>
-        </main>
+                <div className={styles.description}>
+                    <h1>The Future of</h1>
+                    <h1 className={styles["shopping-text"]}>Shopping</h1>
+                    <h1>Is Here</h1>
+                    <p>Experience next-generation e-commerce with our futuristic platform. Discover products that define
+                        tomorrow, today.
+                    </p>
+                    <a href="/shop" className={styles["cyan-btn"]}>Shop Now</a>
+                    <a href="/explore" className={styles["explore-btn"]}>Explore AR</a>
+                </div>
+                <div className={styles["item-preview"]}>
+                    <img
+                        className={styles["preview-image"]}
+                        src="https://public.readdy.ai/ai/img_res/a8dc01dfcfc98e07358a749458fa22b7.jpg"
+                        alt="Futuristic Headphones"
+                    />
+                </div>
+            </main>
+            <Newsletter />
+        </>
     );
 }
