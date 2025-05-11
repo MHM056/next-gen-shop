8c2fa4eb3b8fae61c166115a8eabfa7cadc19255
Mihail <mihailmarholev@gmail.com>
2025-05-11 20:47:39 +0300

    Updated header component
    - sanitized styles;

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -3,33 +3,33 @@ import styles from "./Header.module.css";
 export default function Header() {
     return (
         <header className={styles.header}>
-            <nav className={styles.nav}>
+            <nav>
                 <div className="logo">
                     <span>Next</span><span>Gen</span>
                 </div>
-                <ul className={styles.ul}>
-                    <li><a className={styles.a} href="/">Home</a></li>
-                    <li><a className={styles.a} href="/shop">Shop</a></li>
-                    <li><a className={styles.a} href="/collections">Collections</a></li>
-                    <li><a className={styles.a} href="/about">About</a></li>
-                    <li><a className={styles.a} href="/contact">Contact</a></li>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/shop">Shop</a></li>
+                    <li><a href="/collections">Collections</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
                 </ul>
             </nav>
             <div className={styles["user-nav"]}>
                 <div className={styles["search-container"]}>
-                    <input type="text" placeholder="Search products..." className={styles["search-input"]} />
-                    <i className={`ri-search-line ${styles["search-icon"]} ${styles.i}`}></i>
+                    <input type="text" placeholder="Search products..." />
+                    <i className="ri-search-line"></i>
                 </div>
                 <div>
-                    <i className={`ri-user-line ${styles.i}`}></i>
+                    <i className="ri-user-line"></i>
                     <span>1</span>
                 </div>
                 <div>
-                    <i className={`ri-shopping-cart-line ${styles.i}`}></i>
+                    <i className="ri-shopping-cart-line"></i>
                     <span>2</span>
                 </div>
                 <div>
-                    <i className={`ri-heart-line ${styles.i}`}></i>
+                    <i className="ri-heart-line"></i>
                 </div>
             </div>
         </header>
