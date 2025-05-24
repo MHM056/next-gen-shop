ef036d3d40fe01509c6cef02060a09ba70ed4b2c
Mihail <mihailmarholev@gmail.com>
2025-05-24 20:29:30 +0300

    Updated register page component
    - Upgraded JSX and styles;
    - Fixed minor styling issues;

--- app/register/page.js
+++ app/register/page.js
@@ -3,19 +3,21 @@ import styles from "./page.module.css";
 
 export default function Register() {
     return (
-        <div className={styles.bg}>
-            <img className={styles.background} src="/images/background.jpg"></img>
-            <form className={styles.form}>
-                <div>
-                    <img src="/images/Logo.png" className={styles.logo} />
-                </div>
-                <h1>Register</h1>
-                <input type="text" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <input type="password" placeholder="Repeat Password" />
-                <p>Already registered? <Link href="/login">Login</Link></p>
-                <button>Register</button>
-            </form>
+        <div className={styles["hero-section"]}>
+            <div className={styles.overlay}></div>
+            <div className={styles.container}>
+                <form className={styles.form}>
+                    <div>
+                        <img src="/images/Logo.png" className={styles.logo} />
+                    </div>
+                    <h1>Register</h1>
+                    <input type="text" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Repeat Password" />
+                    <p>Already registered? <Link href="/login">Login</Link></p>
+                    <button>Register</button>
+                </form>
+            </div>
         </div>
     );
 }
