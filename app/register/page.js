b33f70d75564197c316e0645677ce477dc26970d
Mihail <mihailmarholev@gmail.com>
2025-05-29 21:10:54 +0300

    Updated register component
    - added error state;
    - added register handler functionality;

--- app/register/page.js
+++ app/register/page.js
@@ -1,19 +1,43 @@
+"use client";
 import Link from "next/link";
 import styles from "./page.module.css";
+import { useState } from "react";
+import { registerUser } from "@/lib/api/userAuth";
+import { useRouter } from "next/navigation";
 
 export default function Register() {
+    const [error, setError] = useState('');
+    const router = useRouter();
+
+    async function registerHandler(e) {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const email = formData.get('email');
+        const password = formData.get('password');
+        const repeatPassword = formData.get('repeatPassword');
+
+        try {
+            await registerUser({ email, password, repeatPassword });
+            router.push('/');
+        } catch (err) {
+            setError(err.message);
+        }
+    }
+
     return (
         <div className={styles["hero-section"]}>
             <div className={styles.overlay}></div>
             <div className={styles.container}>
-                <form className={styles.form}>
+                <form onSubmit={registerHandler} className={styles.form}>
                     <div>
                         <img src="/images/Logo.png" className={styles.logo} />
                     </div>
                     <h1>Register</h1>
-                    <input type="text" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <input type="password" placeholder="Repeat Password" />
+                    <input name="email" type="text" placeholder="Email" />
+                    <input name="password" type="password" placeholder="Password" />
+                    <input name="repeatPassword" type="password" placeholder="Repeat Password" />
+                    {error && <p>{error}</p>}
                     <p>Already registered? <Link href="/login">Login</Link></p>
                     <button>Register</button>
                 </form>
