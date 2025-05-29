7c1d57cf4ef15192b2ee15ba63b0222651b82d4c
Mihail <mihailmarholev@gmail.com>
2025-05-29 21:10:00 +0300

    Updated register route
    - added repeat password to the post reequest;

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -2,9 +2,16 @@ import { register } from "@/lib/controllers/userController";
 
 export async function POST(req) {
     try {
-        const { email, password } = await req.json();
+        const { email, password, repeatPassword } = await req.json();
+
+        if (password !== repeatPassword) {
+            return Response.json({ error: "Passwords do not match" }, { status: 400 });
+        }
+
         const user = await register({ email, password });
+        
         return Response.json(user, { status: 201 });
+
     } catch (err) {
         return Response.json({ error: err.message }, { status: 400 });
     }
