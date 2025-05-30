f1267a28eeb94c9fe7f18193a3ea6690bafaa6f2
Mihail <mihailmarholev@gmail.com>
2025-05-30 20:17:59 +0300

    Updated admin page component
    - added RecentActivities component;

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,5 +1,6 @@
 import QuickActions from "@/components/admin/quick-actions/QuickActions";
 import styles from "./page.module.css";
+import RecentActivities from "@/components/admin/recent-activities/RecentActivities";
 
 export default function AdminPage() {
     return (
@@ -191,49 +192,7 @@ export default function AdminPage() {
                 </div>
                 <div className={styles["actions-and-activities"]}>
                     <QuickActions />
-                    <div className={styles["recent-activities"]}>
-                        <div>
-                            <h3>Recent Activities</h3>
-                        </div>
-                        <div>
-                            <div className={styles["activity"]}>
-                                <div>
-                                    <i className={`ri-check-line ${styles["order-icon"]}`}></i>
-                                </div>
-                                <div>
-                                    <p className={styles["activity-title"]}>Order #ORD-7843 has been completed</p>
-                                    <p className={styles["activity-time"]}>2 minutes ago</p>
-                                </div>
-                            </div>
-                            <div className={styles["activity"]}>
-                                <div>
-                                    <i className={`ri-user-add-line ${styles["user-icon"]}`}></i>
-                                </div>
-                                <div>
-                                    <p className={styles["activity-title"]}>New user registration: Alexander Thompson</p>
-                                    <p className={styles["activity-time"]}>15 minutes ago</p>
-                                </div>
-                            </div>
-                            <div className={styles["activity"]}>
-                                <div>
-                                    <i className={`ri-price-tag-3-line ${styles["discount-icon"]}`}></i>
-                                </div>
-                                <div>
-                                    <p className={styles["activity-title"]}>New discount code created: SPRING25</p>
-                                    <p className={styles["activity-time"]}>1 hour ago</p>
-                                </div>
-                            </div>
-                            <div className={styles["activity"]}>
-                                <div>
-                                    <i className={`ri-box-3-line ${styles["product-icon"]}`}></i>
-                                </div>
-                                <div>
-                                    <p className={styles["activity-title"]}>New product added: Echo Quantum Speaker</p>
-                                    <p className={styles["activity-time"]}>2 hours ago</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <RecentActivities />
                 </div>
             </div>
         </section >
