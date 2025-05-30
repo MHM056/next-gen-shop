fe39db23d5a19c061c5a929b8f2eada31e014f48
Mihail <mihailmarholev@gmail.com>
2025-05-30 19:48:48 +0300

    Updated admin component
    - added QuickActions component;

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,3 +1,4 @@
+import QuickActions from "@/components/admin/quick-actions/QuickActions";
 import styles from "./page.module.css";
 
 export default function AdminPage() {
@@ -189,37 +190,7 @@ export default function AdminPage() {
                     </div>
                 </div>
                 <div className={styles["actions-and-activities"]}>
-                    <div className={styles["quick-actions"]}>
-                        <div>
-                            <h3>Quick Actions</h3>
-                        </div>
-                        <div className={styles["actions"]}>
-                            <div>
-                                <button>
-                                    <i className="ri-add-circle-line text-primary ri-2x mb-2"></i>
-                                    <h4>Add Product</h4>
-                                    <p>Create a new product listing</p>
-                                </button>
-                                <button>
-                                    <i className="ri-user-add-line text-primary ri-2x mb-2"></i>
-                                    <h4>Add User</h4>
-                                    <p>Create a new user account</p>
-                                </button>
-                            </div>
-                            <div>
-                                <button>
-                                    <i className="ri-price-tag-3-line text-primary ri-2x mb-2"></i>
-                                    <h4>Add Discount</h4>
-                                    <p>Create a new discount code</p>
-                                </button>
-                                <button>
-                                    <i className="ri-settings-line text-primary ri-2x mb-2"></i>
-                                    <h4>Settings</h4>
-                                    <p>Manage store settings</p>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
+                    <QuickActions />
                     <div className={styles["recent-activities"]}>
                         <div>
                             <h3>Recent Activities</h3>
