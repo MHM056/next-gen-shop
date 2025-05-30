59c5ad7e7b69c3f97a405e1b25d86abbc91c1300
Mihail <mihailmarholev@gmail.com>
2025-05-30 20:45:02 +0300

    Updated admin page component
    - added AdminNav component;

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,27 +1,11 @@
+import AdminNav from "@/components/admin/admin-nav/AdminNav";
 import styles from "./page.module.css";
 import ActionsAndActivities from "@/components/admin/actions-and-activities/ActionsAndActivities";
 
 export default function AdminPage() {
     return (
         <section className={styles.admin}>
-            <nav>
-                <div>
-                    <button><i className="ri-close-line"></i></button>
-                    <span className={styles.title}>Admin Panel</span>
-                </div>
-                <div>
-                    <div className={styles["admin-logout"]}>
-                        <div>
-                            <i className="ri-user-line"></i>
-                            <span>Admin</span>
-                        </div>
-                        <div className={styles.logout}>
-                            <i className="ri-logout-box-line"></i>
-                            <a>Logout</a>
-                        </div>
-                    </div>
-                </div>
-            </nav>
+            <AdminNav />
             <div>
                 <div className={styles["general-view"]}>
                     <div className={styles["total-card"]}>
