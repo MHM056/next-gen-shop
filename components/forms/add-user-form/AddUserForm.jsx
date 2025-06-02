51e7531a711a5f84d30e2563c42f126821365e35
Mihail <mihailmarholev@gmail.com>
2025-06-02 21:24:48 +0300

    Updated AddUserForm
    - added user creation functionality;

--- components/forms/add-user-form/AddUserForm.jsx
+++ components/forms/add-user-form/AddUserForm.jsx
@@ -1,13 +1,23 @@
+"use client";
+import { createUser } from "@/lib/api/userAuth";
 import styles from "./AddUserForm.module.css";
+import { useState } from "react";
 
 export default function AddUserForm({ onClose }) {
+    const [error, setError] = useState('');
 
     async function handleSubmit(e) {
         e.preventDefault();
         const formData = new FormData(e.target);
-        console.log(formData);
-        
-        onClose(); 
+        const email = formData.get("email");
+        const password = formData.get("password");
+        const role = formData.get("role");
+        try {
+            await createUser({ email, password, role });
+            onClose();
+        } catch (err) {
+            setError(err.message);
+        }
     };
 
     return (
@@ -18,12 +28,12 @@ export default function AddUserForm({ onClose }) {
             <h1>Add User</h1>
             <input name="email" type="text" placeholder="Email" required />
             <input name="password" type="password" placeholder="Password" required />
-                <select name="role" id="role" className={styles["form-select"]} defaultValue="User">
-                    <option value="user">User</option>
-                    <option value="admin">Admin</option>
-                    <option value="moderator">Moderator</option>
-                </select>
-            {/* {error && <p>{error}</p>} */}
+            <select name="role" id="role" className={styles["form-select"]} defaultValue="User">
+                <option value="user">User</option>
+                <option value="admin">Admin</option>
+                <option value="moderator">Moderator</option>
+            </select>
+            {error && <p>{error}</p>}
             <button type="submit" className={styles["create-btn"]}>Create User</button>
         </form>
     );
