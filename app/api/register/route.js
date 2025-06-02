6a52ab653f148292caf16ca39b35d0d95cd6cecc
Mihail <mihailmarholev@gmail.com>
2025-06-02 21:25:56 +0300

    Updated register route
    - added cookies on user register;

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,18 +1,29 @@
 import { register } from "@/lib/controllers/userController";
+import { NextResponse } from "next/server";
 
 export async function POST(req) {
     try {
         const { email, password, repeatPassword } = await req.json();
 
         if (password !== repeatPassword) {
-            return Response.json({ error: "Passwords do not match" }, { status: 400 });
+            return NextResponse.json({ error: "Passwords do not match" }, { status: 400 });
         }
 
         const user = await register({ email, password });
+
+        cookies().set({
+            name: "token",
+            value: token,
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "development",
+            sameSite: "lax",
+            path: "/",
+            maxAge: 60 * 60 * 24 * 7
+        })
         
-        return Response.json(user, { status: 201 });
+        return NextResponse.json(user, { status: 201 });
 
     } catch (err) {
-        return Response.json({ error: err.message }, { status: 400 });
+        return NextResponse.json({ error: err.message }, { status: 400 });
     }
 }
