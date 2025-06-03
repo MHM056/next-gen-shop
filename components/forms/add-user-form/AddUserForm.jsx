a328ed5ca9969f60713616685244d1da518383c5
Mihail <mihailmarholev@gmail.com>
2025-06-03 21:11:17 +0300

    Updated add-user-form component
    - added state for user creation (submit button changes it's title based on creation state);
    - changed error paragraph color to red;
    - added onAdd data validation function;

--- components/forms/add-user-form/AddUserForm.jsx
+++ components/forms/add-user-form/AddUserForm.jsx
@@ -2,9 +2,11 @@
 import { createUser } from "@/lib/api/userAuth";
 import styles from "./AddUserForm.module.css";
 import { useState } from "react";
+import validateData from "@/lib/utils/validateData";
 
 export default function AddUserForm({ onClose }) {
     const [error, setError] = useState('');
+    const [creatingUser, setCreatingUser] = useState(false);
 
     async function handleSubmit(e) {
         e.preventDefault();
@@ -12,11 +14,15 @@ export default function AddUserForm({ onClose }) {
         const email = formData.get("email");
         const password = formData.get("password");
         const role = formData.get("role");
+
         try {
+            setCreatingUser(true);
+            validateData.onAdd({ email, password, role });
             await createUser({ email, password, role });
             onClose();
         } catch (err) {
             setError(err.message);
+            setCreatingUser(false);
         }
     };
 
@@ -28,13 +34,13 @@ export default function AddUserForm({ onClose }) {
             <h1>Add User</h1>
             <input name="email" type="text" placeholder="Email" required />
             <input name="password" type="password" placeholder="Password" required />
-            <select name="role" id="role" className={styles["form-select"]} defaultValue="User">
+            <select name="role" id="role" className={styles["form-select"]} defaultValue="user">
                 <option value="user">User</option>
                 <option value="admin">Admin</option>
                 <option value="moderator">Moderator</option>
             </select>
-            {error && <p>{error}</p>}
-            <button type="submit" className={styles["create-btn"]}>Create User</button>
+            {error && <p className={styles.error}>{error}</p>}
+            <button type="submit" className={styles["create-btn"]}>{creatingUser ? "Creating..." : "Create User"}</button>
         </form>
     );
 }
