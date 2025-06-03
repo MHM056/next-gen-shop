58a264429a6474ab2b1c7f00cee99965a70c6961
Mihail <mihailmarholev@gmail.com>
2025-06-03 21:52:19 +0300

    Created login component

---
+++ app/(auth)/login/page.js
@@ -0,0 +1,62 @@
+"use client";
+import Link from "next/link";
+import styles from "./page.module.css";
+import { useState } from "react";
+import { loginUser } from "@/lib/api/userAuth";
+import { useRouter } from "next/navigation";
+import validateData from "@/lib/utils/validateData";
+
+export default function Login() {
+    const [error, setError] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [loggingUser, setLoggingUser] = useState(false);
+    const router = useRouter();
+
+    async function loginHandler(e) {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        try {
+            setLoggingUser(true);
+            validateData.onLogin({ email, password });
+            await loginUser({ email, password });
+            router.push('/');
+        } catch (err) {
+            setError(err.message);
+            setLoggingUser(false);
+        }
+    }
+
+    return (
+        <div className={styles["hero-section"]}>
+            <div className={styles.overlay}></div>
+            <div className={styles.container}>
+                <form onSubmit={loginHandler} className={styles.form}>
+                    <div>
+                        <img src="/images/Logo.png" className={styles.logo} />
+                    </div>
+                    <h1>Login</h1>
+                    <input name="email" type="text" placeholder="Email" required />
+                    <div className={styles["input-wrapper"]}>
+                        <input
+                            name="password"
+                            type={showPassword ? "text" : "password"}
+                            placeholder="Password"
+                            required
+                        />
+                        <i className={`ri-eye-${showPassword ? "off-line" : "line"} ${styles["eye-icon"]}`}
+                            onClick={() => setShowPassword(prev => !prev)}
+                        ></i>
+                    </div>
+
+                    {error && <p>{error}</p>}
+                    <p>Don't have an account? <Link href="/register">Register</Link></p>
+                    <button>{loggingUser ? "Logging in..." : "Login"}</button>
+                </form>
+            </div>
+        </div>
+    );
+}
