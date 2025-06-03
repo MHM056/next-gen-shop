d676b2aa279a41c9d99dd91b881840fd3a888ffc
Mihail <mihailmarholev@gmail.com>
2025-06-03 21:52:59 +0300

    Updated api login route
    - cookies are now set on logging in;

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -1,9 +1,22 @@
 import { login } from "@/lib/controllers/userController";
+import { cookies } from "next/headers";
 
 export async function POST(req) {
     try {
         const { email, password } = await req.json();
-        const user = await login({ email, password });
+
+        const { token, ...user } = await login({ email, password });
+
+        cookies().set({
+            name: "token",
+            value: token,
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== "development",
+            sameSite: "lax",
+            path: "/",
+            maxAge: 60 * 60 * 24 * 7
+        });
+
         return Response.json(user, { status: 200 });
     } catch (err) {
         return Response.json({ error: err.message }, { status: 401 });
