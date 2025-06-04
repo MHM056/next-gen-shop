22d366c8f693e38f12d587559b8d755c9b49c444
Mihail <mihailmarholev@gmail.com>
2025-06-04 21:06:43 +0300

    Updated header component

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,6 +1,6 @@
-import Link from "next/link";
 import styles from "./Header.module.css";
 import NavLink from "./Navlink";
+import UserNav from "./UserNav";
 
 export default function Header() {
     return (
@@ -17,23 +17,7 @@ export default function Header() {
                     <li><NavLink href="/contact">Contact</NavLink></li>
                 </ul>
             </nav>
-            <div className={styles["user-nav"]}>
-                <div className={styles["search-container"]}>
-                    <input type="text" placeholder="Search products..." />
-                    <i className="ri-search-line"></i>
-                </div>
-                <div>
-                    <i className="ri-user-line"></i>
-                    <span>1</span>
-                </div>
-                <div>
-                    <i className="ri-shopping-cart-line"></i>
-                    <span>2</span>
-                </div>
-                <div>
-                    <i className="ri-heart-line"></i>
-                </div>
-            </div>
+            <UserNav />
         </header>
     );
 }
