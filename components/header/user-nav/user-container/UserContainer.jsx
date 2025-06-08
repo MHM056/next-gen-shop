e7acbce49538d985a5af2f9b399b4496557b5544
Mihail <mihailmarholev@gmail.com>
2025-06-08 21:27:10 +0300

    Updated user container component
    Added logout functionality

--- components/header/user-nav/user-container/UserContainer.jsx
+++ components/header/user-nav/user-container/UserContainer.jsx
@@ -1,13 +1,26 @@
+"use client";
 import Link from "next/link";
 import styles from "./UserContainer.module.css";
 import { useState } from "react";
+import { logoutUser } from "@/lib/api/userAuth";
+import { useRouter } from "next/navigation";
 
 export default function UserContainer() {
     const [showDropdown, setShowDropdown] = useState(false);
-    const isLoggedIn = false;
+    const router = useRouter();
+    const isLoggedIn = true;
 
     function toggleDropdown() {
         setShowDropdown(prev => !prev);
+    }
+
+    async function logout() {
+        try {
+            await logoutUser();
+            router.push('/');
+        } catch (err) {
+            console.log(err.message);
+        }
     }
     return (
         <div className={styles["user-container"]}>
@@ -17,15 +30,15 @@ export default function UserContainer() {
                 <div className={styles.dropdown} onClick={toggleDropdown}>
                     {!isLoggedIn ? (
                         <>
-                            <Link href="/login"><i className="ri-login-circle-line" /> Login</Link>
-                            <Link href="/register"><i className="ri-user-add-line" /> Register</Link>
+                            <Link href="/login" prefetch={false}><i className="ri-login-circle-line" /> Login</Link>
+                            <Link href="/register" prefetch={false}><i className="ri-user-add-line" /> Register</Link>
                         </>
                     ) : (
                         <>
-                            <Link href="/profile"><i className="ri-user-line" /> My Profile</Link>
-                            <Link href="/orders"><i className="ri-lock-line" /> My Orders</Link>
-                            <Link href="/wishlist"><i className="ri-heart-line" /> Wishlist</Link>
-                            <Link href="/logout"><i className="ri-logout-circle-line" /> Logout</Link>
+                            <Link href="/profile" prefetch={false}><i className="ri-user-line" /> My Profile</Link>
+                            <Link href="/orders" prefetch={false}><i className="ri-lock-line" /> My Orders</Link>
+                            <Link href="/wishlist" prefetch={false}><i className="ri-heart-line" /> Wishlist</Link>
+                            <Link href="" onClick={logout} prefetch={false}><i className="ri-logout-circle-line" /> Logout</Link>
                         </>
                     )}
                 </div>
