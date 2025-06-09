1a2f4acba51e3beea7dadffd3e7344f029cddd2a
Mihail <mihailmarholev@gmail.com>
2025-06-09 20:19:30 +0300

    Updated user container component

--- components/header/user-nav/user-container/UserContainer.jsx
+++ components/header/user-nav/user-container/UserContainer.jsx
@@ -1,12 +1,13 @@
 "use client";
 import Link from "next/link";
 import styles from "./UserContainer.module.css";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { logoutUser } from "@/lib/api/userAuth";
 import { useRouter } from "next/navigation";
 
 export default function UserContainer() {
     const [showDropdown, setShowDropdown] = useState(false);
+    const dropdownRef = useRef(null);
     const router = useRouter();
     const isLoggedIn = true;
 
@@ -14,31 +15,48 @@ export default function UserContainer() {
         setShowDropdown(prev => !prev);
     }
 
-    async function logout() {
+    async function handleLogout() {
         try {
             await logoutUser();
-            router.push('/');
+            router.push("/");
         } catch (err) {
-            console.log(err.message);
+            console.error(err.message);
         }
     }
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (
+                dropdownRef.current &&
+                !dropdownRef.current.contains(event.target)
+            ) {
+                setShowDropdown(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
     return (
-        <div className={styles["user-container"]}>
+        <div className={styles["user-container"]} ref={dropdownRef}>
             <i className="ri-user-line" onClick={toggleDropdown}></i>
             <span>1</span>
             {showDropdown && (
                 <div className={styles.dropdown} onClick={toggleDropdown}>
                     {!isLoggedIn ? (
                         <>
-                            <Link href="/login" prefetch={false}><i className="ri-login-circle-line" /> Login</Link>
-                            <Link href="/register" prefetch={false}><i className="ri-user-add-line" /> Register</Link>
+                            <Link href="/login"><i className="ri-login-circle-line" /> Login</Link>
+                            <Link href="/register"><i className="ri-user-add-line" /> Register</Link>
                         </>
                     ) : (
                         <>
-                            <Link href="/profile" prefetch={false}><i className="ri-user-line" /> My Profile</Link>
-                            <Link href="/orders" prefetch={false}><i className="ri-lock-line" /> My Orders</Link>
-                            <Link href="/wishlist" prefetch={false}><i className="ri-heart-line" /> Wishlist</Link>
-                            <Link href="" onClick={logout} prefetch={false}><i className="ri-logout-circle-line" /> Logout</Link>
+                            <Link href="/profile"><i className="ri-user-line" /> My Profile</Link>
+                            <Link href="/orders"><i className="ri-lock-line" /> My Orders</Link>
+                            <Link href="/wishlist"><i className="ri-heart-line" /> Wishlist</Link>
+                            <Link href="" onClick={handleLogout}><i className="ri-logout-circle-line" /> Logout</Link>
                         </>
                     )}
                 </div>
