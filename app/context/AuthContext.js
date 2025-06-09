a27c80992d5fdd0e11769a95b5e11197ed5e380d
Mihail <mihailmarholev@gmail.com>
2025-06-09 21:51:10 +0300

    Created auth context

---
+++ app/context/AuthContext.js
@@ -0,0 +1,35 @@
+"use client";
+import { getUser } from "@/lib/api/userAuth";
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext(null);
+
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchAuth() {
+            try {
+                const data = await getUser();
+                setUser(data.user);
+            } catch (err) {
+                setUser(null);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchAuth();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user, loading, setUser }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
