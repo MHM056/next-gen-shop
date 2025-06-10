c1d0b6ea2566602e933b66be1a1b99ef0d0b7b4e
Mihail <mihailmarholev@gmail.com>
2025-06-10 19:00:22 +0300

    Updated register route
    - added missing cookies import;

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,5 +1,6 @@
 import { register } from "@/lib/controllers/userController";
 import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export async function POST(req) {
     try {
