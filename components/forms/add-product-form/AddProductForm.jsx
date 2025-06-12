d45c17f3f8926aa42918698785321a41ca6c7227
Mihail <mihailmarholev@gmail.com>
2025-06-12 21:38:38 +0300

    Updated add-product-form component
    - new fields and styles added;

--- components/forms/add-product-form/AddProductForm.jsx
+++ components/forms/add-product-form/AddProductForm.jsx
@@ -33,22 +33,45 @@ export default function AddProductForm({ onClose }) {
             </div>
             <h1>Add Product</h1>
             <div className={styles["item-info"]}>
-                <input name="title" type="text" placeholder="Title" required />
-                <input name="brand" type="text" placeholder="Brand" required />
-                <select name="category" id="category" className={styles["form-select"]} placeholder="Category">
-                    <option value="headphones">Headphones</option>
-                    <option value="earbuds">Earbuds</option>
-                    <option value="speakers">Speakers</option>
-                    <option value="microphones">Microphones</option>
-                    <option value="accessories">Accessories</option>
-                </select>
-                <select name="feature" id="feature" className={styles["form-select"]} placeholder="Feature">
-                    <option value="wireless">Wireless</option>
-                    <option value="noise cancellation">Noise Cancellation</option>
-                    <option value="voice assistant">Voice Assistant</option>
-                    <option value="water resistant">Water Resistant</option>
-                </select>
-                <textarea name="description" id="description"></textarea>
+                <div className={styles["main-info"]}>
+                    <input name="title" type="text" placeholder="Title" required />
+                    <input name="brand" type="text" placeholder="Brand" required />
+                    <input name="quantity" type="number" placeholder="Quantity" min={1} max={100} required />
+                    <input name="price" type="number" placeholder="Price" step={10} min={10} max={90} required />
+                    <div className={styles["select-container"]}>
+                        <div className={styles.select}>
+
+                            <label for="category">Category</label>
+                            <select name="category" id="category" className={styles["form-select"]} placeholder="Category" required>
+                                <option value="headphones">Headphones</option>
+                                <option value="earbuds">Earbuds</option>
+                                <option value="speakers">Speakers</option>
+                                <option value="microphones">Microphones</option>
+                                <option value="accessories">Accessories</option>
+                            </select>
+                        </div>
+                        <div className={styles.select}>
+                            <label for="feature">Feature</label>
+                            <select name="feature" id="feature" className={styles["form-select"]} placeholder="Feature" required>
+                                <option value="wireless">Wireless</option>
+                                <option value="noise cancellation">Noise Cancellation</option>
+                                <option value="voice assistant">Voice Assistant</option>
+                                <option value="water resistant">Water Resistant</option>
+                            </select>
+                        </div>
+                        <div className={styles.select}>
+
+                            <label for="label">Label</label>
+                            <select name="label" id="label" className={styles["form-select"]} placeholder="Label">
+                                <option value="none">None</option>
+                                <option value="new">NEW</option>
+                                <option value="popular">POPULAR</option>
+                                <option value="limited">LIMITED</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <textarea name="description" id="description" placeholder="Description"></textarea>
             </div>
             {error && <p className={styles.error}>{error}</p>}
             <button type="submit" className={styles["create-btn"]}>{creatingProduct ? "Adding..." : "Add Product"}</button>
