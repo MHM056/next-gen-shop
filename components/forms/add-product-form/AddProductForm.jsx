1a61e0b9a78e3d6a8f3f9222916cd497f7c66800
Mihail <mihailmarholev@gmail.com>
2025-06-20 22:07:40 +0300

    Update AddProductForm.jsx

--- components/forms/add-product-form/AddProductForm.jsx
+++ components/forms/add-product-form/AddProductForm.jsx
@@ -11,7 +11,11 @@ export default function AddProductForm({ onClose }) {
     async function handleSubmit(e) {
         e.preventDefault();
         const formData = new FormData(e.target);
-        const color = formData.get("color");
+        const price = formData.get("price");
+        console.log(price);
+        console.log(typeof price);
+        
+        
         try {
             // setCreatingProduct(true);
             // validateData.onAdd({ email, password, role });
@@ -34,7 +38,7 @@ export default function AddProductForm({ onClose }) {
                     <input name="title" type="text" placeholder="Title" required />
                     <input name="brand" type="text" placeholder="Brand" required />
                     <input name="quantity" type="number" placeholder="Quantity" min={1} max={100} required />
-                    <input name="price" type="number" placeholder="Price" step={10} min={10} max={9000} required />
+                    <input name="price" type="number" placeholder="Price" min={10} max={9000} required />
                     <div className={styles["select-container"]}>
                         <div className={styles.select}>
                             <label htmlFor="category">Category</label>
@@ -66,14 +70,14 @@ export default function AddProductForm({ onClose }) {
                         </div>
                     </div>
                     <input name="image" type="text" placeholder="Image" required />
-                    <input name="discount" type="number" placeholder="Discount" min={10} max={80} />
+                    <input name="discount" type="number" placeholder="Discount %" min={10} max={80} />
                     <div className={styles.isFeatured}>
                         <input name="isFeatured" id="isFeatured" type="checkbox" className={styles.checkbox} />
                         <label htmlFor="isFeatured">Is Featured?</label>
                     </div>
                 </div>
 
-                <textarea name="description" id="description" placeholder="Description"></textarea>
+                <textarea name="description" id="description" placeholder="Description" required></textarea>
             </div>
             {error && <p className={styles.error}>{error}</p>}
             <button type="submit" className={styles["create-btn"]}>{creatingProduct ? "Adding..." : "Add Product"}</button>
