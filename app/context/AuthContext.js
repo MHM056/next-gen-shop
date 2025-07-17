ad95a42f585daa66238d29f676adbf32c2d74594
Mihail <mihailmarholev@gmail.com>
2025-07-17 19:20:10 +0300

    Updated auth context

--- app/context/AuthContext.js
+++ app/context/AuthContext.js
@@ -24,7 +24,7 @@ export function AuthProvider({ children }) {
     }, []);
 
     return (
-        <AuthContext.Provider value={{ user, setUser }}>
+        <AuthContext.Provider value={{ user, setUser, loading }}>
             {children}
         </AuthContext.Provider>
     );
