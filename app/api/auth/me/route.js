4a980a4e11040c26e714c932f04d1eb01746432e
Mihail <mihailmarholev@gmail.com>
2025-07-17 19:20:33 +0300

    Updated me route

--- app/api/auth/me/route.js
+++ app/api/auth/me/route.js
@@ -1,8 +1,10 @@
 import { verifyJWT } from "@/lib/jwt";
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-export async function GET(req) {
-    const token = req.cookies.get("token")?.value;
+export async function GET() {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token")?.value;
 
     if (!token) {
         return NextResponse.json({ user: null }, { status: 401 });
