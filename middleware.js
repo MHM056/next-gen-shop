a648fd52ecb8734f21e4f9e079c57fcba2eb28fd
Mihail <mihailmarholev@gmail.com>
2025-07-19 21:55:49 +0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,19 +3,19 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 const protectedRoutes = ["/admin"];
-const publicRoutes = ["/","/login", "/register", "/about"]
+const publicRoutes = ["/", "/login", "/register", "/about"]
 
 export default async function middleware(req) {
     const path = req.nextUrl.pathname;
-    
+
     const isProtectedRoute = protectedRoutes.includes(path);
     const isPublicRoute = publicRoutes.includes(path);
 
     const token = (await cookies()).get("token")?.value;
-    const userData = verifyJWT(token);
-    const isAuth = userData && userData.role === "admin";
-  
+    const userData = await verifyJWT(token);
     
+    const isAuth = userData && userData.role === "admin";
+
 
     if (isProtectedRoute && !isAuth) {
         return NextResponse.redirect(new URL("/login", req.nextUrl))
@@ -23,3 +23,7 @@ export default async function middleware(req) {
 
     return NextResponse.next();
 }
+
+export const config = {
+    matcher: ["/admin/:path*"],
+};
