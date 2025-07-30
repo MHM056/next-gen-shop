377bf0b5d6394432b0645b07694ec54d79a8b9bb
Mihail <mihailmarholev@gmail.com>
2025-07-30 22:01:30 +0300

    Update AdminNav.jsx

--- components/admin/admin-nav/AdminNav.jsx
+++ components/admin/admin-nav/AdminNav.jsx
@@ -1,6 +1,6 @@
 import styles from "./AdminNav.module.css";
 
-export default function AdminNav() {
+export default function AdminNav({ adminName }) {
     return (
         <nav className={styles["admin-nav"]}>
             <div>
@@ -10,7 +10,7 @@ export default function AdminNav() {
                 <div className={styles["admin-logout"]}>
                     <div>
                         <i className="ri-user-line"></i>
-                        <span>Admin</span>
+                        <span>Admin</span> <span>{adminName}</span>
                     </div>
                     <div className={styles.logout}>
                         <i className="ri-logout-box-line"></i>
