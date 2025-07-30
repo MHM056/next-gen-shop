56bdd38115e68edba6576ee70edcd5e7c7d357bb
Mihail <mihailmarholev@gmail.com>
2025-07-30 22:01:28 +0300

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -17,7 +17,7 @@ export default function AdminPage() {
 
     return (
         <section className={styles.admin}>
-            <AdminNav />
+            <AdminNav adminName={user.email} />
             <TotalStats />
             {/* <div className="sales-and-revenue">
                     <div>
