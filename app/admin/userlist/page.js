d7d283f3caa0a449e1f40150c3e20c9cab90aa78
Mihail <mihailmarholev@gmail.com>
2025-08-01 22:23:11 +0300

    Updated userlist component

--- app/admin/userlist/page.js
+++ app/admin/userlist/page.js
@@ -1,7 +1,35 @@
+import styles from "./page.module.css";
+
 export default function UserList() {
     return (
-        <>
+        <div className={styles.userlist}>
             <h1>Userlist</h1>
-        </>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>User Id</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Created At</th>
+                        <th>Rank</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>#ORD-7843</td>
+                        <td>Emily Parker</td>
+                        <td>Quantum Earbuds Pro</td>
+                        <td>$249.99</td>
+                        <td><span>Completed</span></td>
+                        <td>
+                            <button>Delete</button>
+                        </td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
     );
 }
