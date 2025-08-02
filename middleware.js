3bac7fe2f364b1ac75a2301de315d8c23ced769d
Mihail <mihailmarholev@gmail.com>
2025-08-02 16:31:53 +0300

    Updated middleware

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { verifyJWT } from "@/lib/jwt";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-const protectedRoutes = ["/admin"];
+const protectedRoutes = ["/admin", "/admin/userlist"];
 const publicRoutes = ["/", "/login", "/register", "/about"]
 
 export default async function middleware(req) {
@@ -13,9 +13,13 @@ export default async function middleware(req) {
 
     const token = (await cookies()).get("token")?.value;
     const userData = await verifyJWT(token);
-    
+
     const isAuth = userData && userData.role === "admin";
 
+    if (isProtectedRoute && userData && !isAuth) {
+        return NextResponse.redirect(new URL("/", req.nextUrl))
+
+    }
 
     if (isProtectedRoute && !isAuth) {
         return NextResponse.redirect(new URL("/login", req.nextUrl))
