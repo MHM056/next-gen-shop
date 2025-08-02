27c7ec3c149f1e74083bbc24d355936a54ad37f1
Mihail <mihailmarholev@gmail.com>
2025-08-02 23:10:39 +0300

    Updated userlist component

--- app/admin/userlist/page.js
+++ app/admin/userlist/page.js
@@ -1,6 +1,19 @@
+import { getUsers } from "@/lib/api/userAuth";
 import styles from "./page.module.css";
 
-export default function UserList() {
+export default async function UserList() {
+
+    try {
+        const users = await getUsers();
+        console.log(users);
+        
+    } catch (error) {
+        console.log(error.message);
+        
+    }
+
+
+
     return (
         <div className={styles.userlist}>
             <h1>Userlist</h1>
@@ -13,6 +26,7 @@ export default function UserList() {
                         <th>Role</th>
                         <th>Created At</th>
                         <th>Rank</th>
+                        <th>Profile</th>
                         <th></th>
                     </tr>
                 </thead>
@@ -23,6 +37,7 @@ export default function UserList() {
                         <td>Quantum Earbuds Pro</td>
                         <td>$249.99</td>
                         <td><span>Completed</span></td>
+                        <td>View</td>
                         <td>
                             <button>Delete</button>
                         </td>
