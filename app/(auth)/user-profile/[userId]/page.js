2eedf486dc46b5f7607718d7f152855317ffa53e
Mihail <mihailmarholev@gmail.com>
2025-08-16 22:12:24 +0300

    Created user-profile router component

---
+++ app/(auth)/user-profile/[userId]/page.js
@@ -0,0 +1,12 @@
+import { useParams } from "next/navigation";
+
+
+export default async function UserProfile({ params}) {
+
+
+    return (
+        <div>
+            <h1>Profile Page</h1>
+        </div>
+    )
+}
